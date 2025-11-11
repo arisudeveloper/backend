c8c640167eab14b47f3427d302b6c6109f141b23
arisudeveloper <alicesantossilva345@gmail.com>
2025-11-11 00:20:15 -0300

    Alteração para build

--- prisma/migrations/20251111031903_init/migration.sql
+++ prisma/migrations/20251111031903_init/migration.sql
@@ -1,6 +1,12 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'approved');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'subscriber');
+
 -- CreateTable
 CREATE TABLE "Users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "First Name" TEXT NOT NULL,
     "Last Name" TEXT NOT NULL,
     "Email" TEXT NOT NULL,
@@ -14,7 +20,12 @@ CREATE TABLE "Users" (
     "Fit" BOOLEAN NOT NULL,
     "Groups" BOOLEAN NOT NULL,
     "Guaranteed" BOOLEAN NOT NULL,
-    "Leisure" BOOLEAN NOT NULL
+    "Leisure" BOOLEAN NOT NULL,
+    "Role" "Role" NOT NULL DEFAULT 'subscriber',
+    "Status" "Status" NOT NULL DEFAULT 'pending',
+    "Policy" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
