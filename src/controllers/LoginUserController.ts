d8915f58357eb41046d92d51c60c80499a27b285
arisudeveloper <alicesantossilva345@gmail.com>
2025-11-11 19:52:33 -0300

    Permissao

--- src/controllers/LoginUserController.ts
+++ src/controllers/LoginUserController.ts
@@ -19,7 +19,7 @@ export default class LoginUserController {
             secure: false,
             path: '/',
             maxAge: 24 * 60 * 60 * 1000,
-            sameSite: 'none',
+            sameSite: 'lax',
           })
           .json({
             firstname: authResult.user.firstname,
