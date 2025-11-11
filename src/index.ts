9f48bb5daadb229a2d06f7fffc033d8f8c70d2c5
arisudeveloper <alicesantossilva345@gmail.com>
2025-11-11 19:37:09 -0300

    Alteração de cors

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(rateLimiter);
 app.use(throttle);
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
