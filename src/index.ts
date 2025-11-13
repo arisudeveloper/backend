7993290faa1856164849c49e586c61634af975f6
arisudeveloper <alicesantossilva345@gmail.com>
2025-11-12 22:35:08 -0300

    Alteração

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(rateLimiter);
 app.use(throttle);
 app.use(
   cors({
-    origin: 'https://somewhere-to.com',
+    origin: 'https://dev.gabrieleustaquio.com.br',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
